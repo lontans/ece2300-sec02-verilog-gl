7517448f8d3565c7cb798e051511299db163400b
js3773 <js3773@cornell.edu>
2026-01-30 10:52:31 -0500

    finished implementation

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -13,7 +13,14 @@ module PairTripleDetector_GL
   input  wire in1,
   input  wire in2,
   output wire out
- );
+);
+  wire w;
+  wire x;
+  wire y;
+  or(w,in0,in1);
+  and(x,in0,in1);
+  and(y,w,in2);
+  or(out,y,x);
 
   //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement pair/triple detector using explicit gate-level modeling
